3d895dd7e60dfd294a1f4d2a5f3d5175f2c8f50a
Francesco Martini <75032755+f-martini@users.noreply.github.com>
2025-07-08 19:00:23 +0200

    Clean up after conflicts resolution

--- src/quaos/circuits/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Circuits import Circuit
-from .Gates import Gate, SUM, SWAP, CNOT, Hadamard, PHASE, ArbitraryGate
-
-__all__ = ["Circuit", "Gate", "SUM", "SWAP", "CNOT", "Hadamard", "PHASE", "ArbitraryGate"]
