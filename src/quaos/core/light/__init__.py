a2c2d3b4b0602cf7f29ac110f2cc3334064d59aa
Francesco Martini <75032755+f-martini@users.noreply.github.com>
2025-10-06 15:35:45 +0200

    updated import strategy

--- src/quaos/core/light/__init__.py
+++ src/quaos/core/light/__init__.py
@@ -1,17 +1,3 @@
-import os
-import ctypes
-import glob
-
-lib_dir = os.path.dirname(__file__)
-deps_dir = lib_dir + "\\.libs"
-if os.name == "nt":
-    os.add_dll_directory(lib_dir)
-    os.add_dll_directory(deps_dir)
-else:
-    os.environ["LD_LIBRARY_PATH"] = lib_dir + ":" + deps_dir + ":" + os.environ.get("LD_LIBRARY_PATH", "")
-    for so_file in glob.glob(os.path.join(lib_dir, "*.so")) + glob.glob(os.path.join(deps_dir, "*.so")):
-        ctypes.CDLL(so_file)
-
 from .light import Aquire
 
 __all__ = ["Aquire"]
