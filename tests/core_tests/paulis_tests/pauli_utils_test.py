2413ee65a85956b18d1e8396cdee65a50f1cf8c5
Rick Simon <rick.paul.axel.simon@gmail.com>
2025-11-25 14:33:16 +0000

    updated make_hermitian test

--- tests/core_tests/paulis_tests/pauli_utils_test.py
+++ tests/core_tests/paulis_tests/pauli_utils_test.py
@@ -8,73 +8,27 @@ from sympleq.core.measurement.allocation import weight_to_phase
 class TestPauliUtils:
 
     def test_make_hermitian(self):
-        # random qubit PauliSums
-        for i in range(50):
+        available_dimensions = [2, 3, 5, 7]
+        # with random phases
+        for i in range(200):
             n_qubits = np.random.randint(1, 10)
+            dims = [np.random.choice(available_dimensions) for _ in range(n_qubits)]
             n_paulis = np.random.randint(1, np.min([4**n_qubits - 1, 10]))
             P = PauliSum.from_random(n_paulis=n_paulis,
-                                     dimensions=[2] * n_qubits,
+                                     dimensions=dims,
                                      rand_weights=True)
-            P.set_phases(np.random.randint(0, 4, n_paulis))
-            P1 = make_hermitian(P)
-            assert P1.is_hermitian(), "make_hermitian failed for random qubit PauliSum"
-
-        # random pure qutrit PauliSums
-        for i in range(50):
-            n_qubits = np.random.randint(1, 6)
-            n_paulis = np.random.randint(1, 8)
-            P = PauliSum.from_random(n_paulis=n_paulis,
-                                     dimensions=[3] * n_qubits,
-                                     rand_weights=True)
-            P.set_weights(np.random.random(n_paulis) + 1j * np.random.random(n_paulis))
-            P.set_phases(np.random.randint(0, 6, n_paulis))
+            P.set_phases(np.random.randint(0, 2 * P.lcm, n_paulis))
             P1 = make_hermitian(P)
             assert P1.is_hermitian()
 
-        # random pure ququint PauliSums
-        for i in range(50):
-            n_qubits = np.random.randint(1, 5)
-            n_paulis = np.random.randint(1, 8)
+        # without random phases
+        for i in range(200):
+            n_qubits = np.random.randint(1, 10)
+            dims = [np.random.choice(available_dimensions) for _ in range(n_qubits)]
+            n_paulis = np.random.randint(1, np.min([4**n_qubits - 1, 10]))
             P = PauliSum.from_random(n_paulis=n_paulis,
-                                     dimensions=[5] * n_qubits,
+                                     dimensions=dims,
                                      rand_weights=True)
-            P.set_weights(np.random.random(n_paulis) + 1j * np.random.random(n_paulis))
-            P.set_phases(np.random.randint(0, 10, n_paulis))
-            P1 = make_hermitian(P)
-            assert P1.is_hermitian()
-
-        # random pure qusept PauliSums
-        for i in range(50):
-            n_qubits = np.random.randint(1, 4)
-            n_paulis = np.random.randint(1, 8)
-            P = PauliSum.from_random(n_paulis=n_paulis,
-                                     dimensions=[7] * n_qubits,
-                                     rand_weights=True)
-            P.set_weights(np.random.random(n_paulis) + 1j * np.random.random(n_paulis))
-            P.set_phases(np.random.randint(0, 14, n_paulis))
-            P1 = make_hermitian(P)
-            assert P1.is_hermitian()
-
-        # random mixed dimension PauliSums
-        for i in range(50):
-            n_qubits = 3
-            n_paulis = np.random.randint(1, 8)
-            P = PauliSum.from_random(n_paulis=n_paulis,
-                                     dimensions=[2, 2, 3],
-                                     rand_weights=True)
-            P.set_weights(np.random.random(n_paulis) + 1j * np.random.random(n_paulis))
-            P.set_phases(np.random.randint(0, 12, n_paulis))
-            P1 = make_hermitian(P)
-            assert P1.is_hermitian()
-
-        for i in range(50):
-            n_qubits = 3
-            n_paulis = np.random.randint(1, 8)
-            P = PauliSum.from_random(n_paulis=n_paulis,
-                                     dimensions=[2, 3, 5],
-                                     rand_weights=True)
-            P.set_weights(np.random.random(n_paulis) + 1j * np.random.random(n_paulis))
-            P.set_weights(np.random.randint(0, 60, n_paulis))
             P1 = make_hermitian(P)
             assert P1.is_hermitian()
 
