79e58193154d51705ef83bc9d848808fe7032c86
Rick Simon <rick.paul.axel.simon@gmail.com>
2026-01-26 15:18:04 +0000

    doc strings

--- src/sympleq/core/statistic_utils.py
+++ src/sympleq/core/statistic_utils.py
@@ -3,11 +3,31 @@ from sympleq.core.paulis import PauliSum
 
 
 def true_mean(H: PauliSum, psi) -> float:
+    """
+    Calculate the true mean of a Hamiltonian given a PauliSum object and a state vector.
+
+    Parameters:
+        H (PauliSum): The PauliSum object representing the Hamiltonian.
+        psi (numpy.ndarray): The state vector.
+
+    Returns:
+        float: The true mean of the Hamiltonian.
+    """
     mu = np.real(np.transpose(np.conjugate(psi)) @ H.to_hilbert_space() @ psi)
     return mu
 
 
 def true_covariance_graph(H: PauliSum, psi) -> np.ndarray:
+    """
+    Calculate the true covariance matrix of a Hamiltonian given a PauliSum object and a state vector.
+
+    Parameters:
+        H (PauliSum): The PauliSum object representing the Hamiltonian.
+        psi (numpy.ndarray): The state vector.
+
+    Returns:
+        numpy.ndarray: The true covariance matrix of the Hamiltonian.
+    """
     p = H.n_paulis()
     mm = [H.to_hilbert_space(i) for i in range(p)]
     psi_dag = psi.conj().T
