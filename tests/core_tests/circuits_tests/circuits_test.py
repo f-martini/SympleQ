d091a127c4c690d3bf9bdd3c335d9d6e5d524c7c
ricott1 <ricott2@gmail.com>
2026-02-01 20:06:45 +0100

    Add test for gates_layout

--- tests/core_tests/circuits_tests/circuits_test.py
+++ tests/core_tests/circuits_tests/circuits_test.py
@@ -401,6 +401,19 @@ class TestCircuits():
                 U_g = g.unitary(dims=dimensions).toarray()
                 assert np.allclose(U_c, U_g)
 
+    def test_gates_layout(self):
+        dimensions = [3] * 4
+        circuit = Circuit.from_random(np.random.randint(4, 12), dimensions)
+
+        # Just check that it doesn't crash
+        _ = circuit.gates_layout()
+        _ = circuit.gates_layout(with_qudit_indices=True)
+        ps_in = PauliSum.from_random(len(dimensions), dimensions)
+        _ = circuit.gates_layout(with_qudit_indices=True, with_input=ps_in)
+        ps_out = PauliSum.from_random(len(dimensions), dimensions)
+        _ = circuit.gates_layout(with_qudit_indices=True, with_input=ps_in, with_output=ps_out)
+        _ = circuit.gates_layout(with_qudit_indices=True, with_input=ps_in, with_output=ps_out, wrap=False)
+
 
 if __name__ == '__main__':
     TestCircuits().test_circuit_unitary()
