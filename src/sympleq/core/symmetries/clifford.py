5b5d7227d616d99d52224f83e90af2a1798eeade
charlie nation <c-nation@users.noreply.github.com>
2026-02-04 17:27:49 +0000

    removing some parts for decomposition pr

--- src/sympleq/core/symmetries/clifford.py
+++ src/sympleq/core/symmetries/clifford.py
@@ -1,74 +1,7 @@
 # import numpy as np
-from sympleq.core.circuits import Gate  # , Circuit
+from sympleq.core.circuits import Gate
 from sympleq.core.paulis import PauliSum
 from sympleq.core.graphs.graph_automorphism import clifford_graph_automorphism_search
-# from sympleq.core.symmetries.phase_correction import clifford_phase_decomposition
-# from sympleq.core.symmetries.block_decomposition import block_decompose, ordered_block_sizes
-
-
-# def min_qudit_clifford_symmetry(pauli_sum: PauliSum,
-#                                 ) -> tuple[Gate, Gate, Gate]:
-#     """
-#     Find a single Clifford symmetry g of the given PauliSum, and decompose it into blocks with a minimal qudit cost
-#     via a symplectic similarity transform: g = T S T^{-1}.
-
-#     :param pauli_sum: Input Hamiltonian
-#     :type pauli_sum: PauliSum
-#     :return: Symmetry gate, minimal block symplectic S, similarity transform T
-#     :type: tuple[Gate, Gate, Gate]
-#     """
-
-#     G = find_clifford_symmetries(pauli_sum, num_symmetries=1,
-#                                  dynamic_refine_every=0)
-#     g = G[0]
-
-#     S, T = block_decompose(g.symplectic, int(pauli_sum.lcm), min_block_size=4)
-#     h_S, h_T = clifford_phase_decomposition(g.symplectic, g.phase_vector, S, T, int(pauli_sum.lcm))
-#     S_gate = Gate('S', g.qudit_indices, S, g.dimensions, h_S)
-#     T_gate = Gate('T', g.qudit_indices, T, g.dimensions, h_T)
-
-#     return g, S_gate, T_gate
-
-
-# def multiple_min_qudit_clifford_symmetries(pauli_sum: PauliSum,
-#                                            n_symmetries: int = 1,
-#                                            ) -> tuple[list[Gate], list[Gate], list[Gate]]:
-#     """
-#     Find multiple Clifford symmetries of the given PauliSum, and decompose each into blocks via symplectic similarity
-
-#     """
-#     G = find_clifford_symmetries(pauli_sum, num_symmetries=n_symmetries,
-#                                  dynamic_refine_every=0)
-
-#     Ss = []
-#     Ts = []
-#     for i, g in enumerate(G):
-#         S, T = block_decompose(g.symplectic, pauli_sum.lcm)
-#         h_S, h_T = clifford_phase_decomposition(g.symplectic, g.phase_vector, S, T, int(pauli_sum.lcm))
-#         S_gate = Gate(f'S{i}', g.qudit_indices, S, g.dimensions, h_S)
-#         T_gate = Gate(f'T{i}', g.qudit_indices, T, g.dimensions, h_T)
-#         Ss.append(S_gate)
-#         Ts.append(T_gate)
-
-#     return G, Ss, Ts
-
-
-# def block_structure(gate: Gate):
-#     symp = gate.symplectic
-#     sizes = np.asarray(ordered_block_sizes(symp, int(gate.lcm)), dtype=int) / 2
-#     return sizes
-
-
-# def qudit_cost(gate: Gate):
-#     return int(max(block_structure(gate)))
-
-
-# def _labels_union(independent: list[int], dependencies: dict[int, list[tuple[int, int]]]) -> list[int]:
-#     """Given a set of independent indices and dependencies (mapping from index to list of (index, label) tuples),
-#     return the sorted union of all involved indices.
-#     check if needed - move to utils if so
-# """
-#     return sorted(set(independent) | set(dependencies.keys()))
 
 
 def find_clifford_symmetries(
